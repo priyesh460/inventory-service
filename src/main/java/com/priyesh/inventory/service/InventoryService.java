5e330566ac0d4f9db19aa39706df19bf37731607
Priyesh Bhagat <priyesh.bhagat@naaptoldc.local>
2024-08-23 16:01:33 +0530

    stable version 1

--- src/main/java/com/priyesh/inventory/service/InventoryService.java
+++ src/main/java/com/priyesh/inventory/service/InventoryService.java
@@ -5,15 +5,18 @@ import org.springframework.stereotype.Service;
 import com.priyesh.inventory.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 	
 	private final InventoryRepository inventoryRepository;
 	
 	public boolean isInStock(String skuCode,Integer quantity)
 	{
+		log.info("Checking Product in Stock for Skucode {}",skuCode);
 		return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
 	}
 
